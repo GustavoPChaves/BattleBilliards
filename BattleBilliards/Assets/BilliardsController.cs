310cb268825799ac062363bf0414a8e0693edb49
Gustavo Chaves <gustavochaves@10-45-48-71.0200.ifce.br.iosda.org>
2019-10-31 15:54:17 -0300

    Basic level, Camera Perspective, Refine input for new camera

--- BattleBilliards/Assets/BilliardsController.cs
+++ BattleBilliards/Assets/BilliardsController.cs
@@ -8,7 +8,7 @@ public class BilliardsController : MonoBehaviour
     private float defaultForce = 100;
     private Rigidbody rb;
 
-    private Vector2 initialPos, finalPos;
+    private Vector3 initialPos, finalPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,29 +20,42 @@ public class BilliardsController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            TouchBegan(mousePos);
+            Vector3 mousePos = MousePosition();
+            TouchBegan(InvertYZ(mousePos));
         }
         if (Input.GetMouseButtonUp(0))
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            TouchEnded(mousePos);
+            Vector3 mousePos = MousePosition();
+            TouchEnded(InvertYZ(mousePos));
         }
     }
 
-    void TouchBegan(Vector2 position)
+    void TouchBegan(Vector3 position)
     {
         rb.velocity = Vector3.zero;
         initialPos = position;
     }
 
-    void TouchEnded(Vector2 position)
+    void TouchEnded(Vector3 position)
     {
         finalPos = position;
 
         var direction = - (finalPos - initialPos).normalized;
         var force = Vector3.Distance(finalPos, initialPos);
         rb.AddForce(direction * defaultForce * force);
+    }
+
+    Vector3 MousePosition()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        var point = Camera.main.ScreenToWorldPoint(mousePos);
+        return point;
+    }
+
+    Vector3 InvertYZ(Vector3 vector)
+    {
+        return new Vector3(vector.x, 0, vector.y);
     }
         
 }
